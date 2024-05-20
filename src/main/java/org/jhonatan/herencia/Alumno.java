150a4f8115c4a2a510972714a763d13c213bb965
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-19 23:20:45 -0500

    getters y setters para las clases profesor y alumno

--- src/main/java/org/jhonatan/herencia/Alumno.java
+++ src/main/java/org/jhonatan/herencia/Alumno.java
@@ -10,4 +10,37 @@ public class Alumno extends Persona {//clase hija de la clase persona
     private double notaMatematicas;
     private double notaCastellano;
     private double notaHistoria;
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public double getNotaMatematicas() {
+        return notaMatematicas;
+    }
+
+    public void setNotaMatematicas(double notaMatematicas) {
+        this.notaMatematicas = notaMatematicas;
+    }
+
+    public double getNotaCastellano() {
+        return notaCastellano;
+    }
+
+    public void setNotaCastellano(double notaCastellano) {
+        this.notaCastellano = notaCastellano;
+    }
+
+    public double getNotaHistoria() {
+        return notaHistoria;
+    }
+
+    public void setNotaHistoria(double notaHistoria) {
+        this.notaHistoria = notaHistoria;
+    }
+
 }
--- src/main/java/org/jhonatan/herencia/Profesor.java
+++ src/main/java/org/jhonatan/herencia/Profesor.java
@@ -4,7 +4,16 @@ package org.jhonatan.herencia;
  *
  * @author Jhonatan
  */
-public class Profesor extends Persona {
+public class Profesor extends Persona {//clase hija de persona
 
     private String asignatura;
+
+    public String getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+
 }
