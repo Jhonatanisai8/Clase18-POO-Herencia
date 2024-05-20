8e7abbaa2b8de1845ee40359f22f6f2ef5efc6fd
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-19 23:31:41 -0500

    creacion objetos en la clase main de las clases alumno y profesor

--- src/main/java/org/jhonatan/herencia/main.java
+++ src/main/java/org/jhonatan/herencia/main.java
@@ -7,6 +7,20 @@ package org.jhonatan.herencia;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("HERENCIA EN POO");
+        Alumno alumno = new Alumno();
+        alumno.setNombre("Jhonatan Isai");
+        alumno.setApellido("Ojeda Sanchez");
+
+        Profesor profesor = new Profesor();
+        profesor.setNombre("Leandro Madrid");
+        profesor.setApellido("Perez");
+        profesor.setAsignatura("Fundamentos de Programación");
+
+        System.out.println("Alumno: " + alumno.getNombre() + " " + alumno.getApellido());
+
+        System.out.println("Profesor " + profesor.getAsignatura() + " :"
+                + profesor.getNombre() + " " + profesor.getApellido());
+
     }
 }
