62de72615d2231464142ad3b7a4b3a65bed15db4
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-01 01:02:38 -0500

    modificando la clase main

--- src/main/java/org/jhonatan/ejemplo/main.java
+++ src/main/java/org/jhonatan/ejemplo/main.java
@@ -13,26 +13,21 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("HERENCIA EN POO");
-        Persona alumno = new Alumno();
-
-        //marca error por que las clases estan en otra clase
-        alumno.setNombre("Jhonatan Isai");
-        alumno.setApellido("Ojeda Sanchez");
-
-        //casteando el objeto de tipo persona
-        ((Alumno) alumno).setInstitucion("Instituto Nacional");
+        System.out.println("=======Instancia de Alumno=======");
 
         Profesor profesor = new Profesor();
         profesor.setNombre("Leandro Madrid");
         profesor.setApellido("Perez");
         profesor.setAsignatura("Fundamentos de Programación");
 
+        System.out.println("=======Instancia de Persona=======");
         Persona persona = new Persona();
         persona.setNombre("Pedro Luis");
         persona.setApellido("Flores Rios");
         persona.setEdad(12);
         persona.setEmail("pedroluis@gmail.com");
 
+        System.out.println("=======Instancia de Alumno=======");
         //objeto de la clase alumno
         Alumno alumnoDani = new Alumno();
         alumnoDani.setNombre("Daniel Eli");
@@ -43,6 +38,7 @@ public class main {
         alumnoDani.setNotaCastellano(12.3);
         alumnoDani.setNotaHistoria(11.2);
         alumnoDani.setNotaMatematicas(9.4);
+        System.out.println("=======Instancia de Alumno Internacional=======");
 
         //creacion de un objeto de la clase alumno internacional
         AlumnoInternacional alumnoJose = new AlumnoInternacional();
@@ -56,17 +52,16 @@ public class main {
         alumnoJose.setNotaHistoria(11.4);
         alumnoJose.setNotaMatematicas(16.7);
 
-        System.out.println("Alumno: " + alumno.getNombre() + " " + alumno.getApellido()
-                + "\nInstitucion: " + ((Alumno) alumno).getInstitucion());
-
+        System.out.println("======================");
         System.out.println("Profesor " + profesor.getAsignatura() + " :"
                 + profesor.getNombre() + " " + profesor.getApellido());
 
+        System.out.println("======================");
         System.out.println("Persona: " + persona.getNombre() + " " + persona.getApellido()
                 + "\nEdad: " + persona.getEdad()
                 + "\nEmail: " + persona.getEmail());
 
-        System.out.println("");
+        System.out.println("======================");
         System.out.println("Alumno"
                 + "\nNombre: " + alumnoDani.getNombre()
                 + "\nApellido: " + alumnoDani.getApellido()
@@ -77,7 +72,7 @@ public class main {
                 + "\nNota historia: " + alumnoDani.getNotaHistoria()
                 + "\nNota Matematicas: " + alumnoDani.getNombre());
 
-        System.out.println("");
+        System.out.println("======================");
         System.out.println("Alumno Internacional"
                 + "\nNombre: " + alumnoJose.getNombre()
                 + "\nApellido: " + alumnoJose.getApellido()
@@ -89,6 +84,7 @@ public class main {
                 + "\nNota historia: " + alumnoJose.getNotaHistoria()
                 + "\nNota Matematicas: " + alumnoJose.getNombre());
 
+        System.out.println("======================");
         System.out.println("\nMostrando Jerarquia de Clases");
         //mostramos la jerarquia de las clases
         Class clase = alumnoJose.getClass();
@@ -98,6 +94,21 @@ public class main {
             System.out.println(hija + " es una clase hija de la clase " + padre);
             clase = clase.getSuperclass();
         }
+
+    }
+
+    public static void creacionObjetoQuenoEssuClase() {
+        Persona alumno = new Alumno();
+
+        //marca error por que las clases estan en otra clase
+        alumno.setNombre("Jhonatan Isai");
+        alumno.setApellido("Ojeda Sanchez");
+
+        //casteando el objeto de tipo persona
+        ((Alumno) alumno).setInstitucion("Instituto Nacional");
+
+        System.out.println("Alumno: " + alumno.getNombre() + " " + alumno.getApellido()
+                + "\nInstitucion: " + ((Alumno) alumno).getInstitucion());
 
     }
 }
