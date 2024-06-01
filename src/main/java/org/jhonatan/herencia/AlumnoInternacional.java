65307c0ef60552edc50b085e26b8856084830b75
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-01 00:29:35 -0500

    nueva clase ALUMNO-INTERNACIONAL que hereda de la clase ALUMNO

---
+++ src/main/java/org/jhonatan/herencia/AlumnoInternacional.java
@@ -0,0 +1,28 @@
+package org.jhonatan.herencia;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class AlumnoInternacional extends Alumno {//hereda de alumno
+
+    private String pais;
+    private double notaIdiamas;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public double getNotaIdiamas() {
+        return notaIdiamas;
+    }
+
+    public void setNotaIdiamas(double notaIdiamas) {
+        this.notaIdiamas = notaIdiamas;
+    }
+
+}
